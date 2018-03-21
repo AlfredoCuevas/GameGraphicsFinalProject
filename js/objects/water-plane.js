58857593c6c745dd4fc091d7d6dca02b50b74711
AlfredoCuevas <cuevasalfredo0@gmail.com>
2018-03-20 17:15:17 -0700

    used a dudv map to create a ripple effect in the water. Then placed a blue tint on the water.

--- js/objects/water-plane.js
+++ js/objects/water-plane.js
@@ -2,13 +2,16 @@ function createWaterPlane(w, d, bufferTextureRefraction, bufferTextureReflection
 	
 	var geometry = new THREE.PlaneGeometry(w, d, 20, 20);
 
-	//var tex = new THREE.TextureLoader().load(texPath);
+	var dudvTex = new THREE.TextureLoader().load(texPath);
+
 
 	var shaderInput = {
 		uRefraction: {type: "t", value: bufferTextureRefraction },
 		uReflection: {type: "t", value: bufferTextureReflection },
+		dudvMap: {type: "t", value: dudvTex },
+		moveFactor: {type: 'f', value: 0.0 }, 
 		uTime: {type: 'f', value: 0.0 },
-		uColor: {type: 'f', value: new THREE.Color('#0051da') },
+		uColor: {type: 'f', value: new THREE.Color('#0098af') },
 	}
 
 	var material = new THREE.ShaderMaterial({
@@ -23,10 +26,17 @@ function createWaterPlane(w, d, bufferTextureRefraction, bufferTextureReflection
 	mesh.Start = function(){
 		//mesh.position.z = -10.0;
 		mesh.rotateX(-Math.PI/2.0);
+
+		// By default, textures have "Clamp To Edge" wrapping mode, which means u or v
+        // over 1 will still be 1 instead of wrapping back to 0.
+        // To fix this, you need to set the wrapping mode of your texture to "Repeat".
+		mesh.material.uniforms.dudvMap.value.wrapS = 
+		mesh.material.uniforms.dudvMap.value.wrapT = THREE.RepeatWrapping;
 	}
 
 	mesh.Update = function(){
-
+		mesh.material.uniforms.moveFactor.value += 0.001; // wave speed movement
+		mesh.material.uniforms.moveFactor.value %= 1;
 	}
 
 	return mesh;
--- shaders/water-shader.js
+++ shaders/water-shader.js
@@ -4,7 +4,7 @@ var waterVShader = `
     varying vec4 clipSpace;
 
     void main(){
-        vUv = uv;
+        vUv = uv * 6.0;
         clipSpace = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
         gl_Position = clipSpace;
     }
@@ -17,15 +17,33 @@ var waterFShader = `
     uniform vec3 uColor;
     uniform sampler2D uRefraction;
     uniform sampler2D uReflection;
+    uniform sampler2D dudvMap;
+
+    uniform float moveFactor; // how fast the water seems to move
+
+    const float waveStrength = 0.01; // how much the water ripples
 
     void main(){
         vec2 ndc = (clipSpace.xy/ clipSpace.w)/2.0 + 0.5;
         vec2 refractionTexCoords = vec2(ndc.x, ndc.y);
         vec2 reflectionTexCoords = vec2(ndc.x, 1.0 - ndc.y); // I need to flip the texture because it is a reflection
 
+        // Add the ripples by distorting the water and applying it to the Tex coordinates
+        vec2 distortion1 = (texture2D(dudvMap, vec2(vUv.x + moveFactor, vUv.y)).rg) * waveStrength;
+        vec2 distortion2 = (texture2D(dudvMap, vec2(-vUv.x + moveFactor, vUv.y + moveFactor)).rg) * waveStrength;
+        vec2 totalDistortion = distortion1 + distortion2;
+
+        refractionTexCoords += totalDistortion;
+        refractionTexCoords = clamp(refractionTexCoords, 0.001, 0.999 );
+
+        reflectionTexCoords += totalDistortion;
+        reflectionTexCoords = clamp(reflectionTexCoords, 0.001, 0.999);
+
+        // Use the distorted texture coordinates to read from the FBO textures
         vec4 refractionColor = texture2D(uRefraction, refractionTexCoords);
         vec4 reflectionColor = texture2D(uReflection, reflectionTexCoords);
 
         gl_FragColor = mix(reflectionColor, refractionColor, 0.5);
+        gl_FragColor = mix(gl_FragColor, vec4(uColor, 1.0), 0.5); // adding a blue tint
     }
 `;
