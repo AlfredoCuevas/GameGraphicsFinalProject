c778a830d2f62760a94ff5aa3e888db9a97bee1c
Trenten <tkaufeld@ucsc.edu>
2018-03-23 19:38:00 -0700

    dont overwrite
    d4gtfg

--- js/objects/cloud-scape.js
+++ js/objects/cloud-scape.js
@@ -1,9 +1,14 @@
-function createClouds(x,y,h)
+function createClouds(x,y,h,clouds)
 {
   var plane = new THREE.PlaneGeometry(x,y);
 
   var shaderInput = {
-    color: 0xFFFFFF
+    moveFactor: {type: 'f', value: 0.0 },
+    uTime: {type: 'f', value: 0.0 },
+    in_val:  { type: "f", value: 0.0 },
+    displaceAmt: { type: "f", value: 0.0 }
+
+
   }
 
   var material = new THREE.ShaderMaterial({
@@ -11,8 +16,8 @@ function createClouds(x,y,h)
         vertexShader: CloudMap,
         fragmentShader: CloudColors,
         side: THREE.DoubleSide,
-        color: 0xFFFFFF,
         clipping: false,
+        map: clouds
       });
 
   var mesh = new THREE.Mesh(plane, material);
