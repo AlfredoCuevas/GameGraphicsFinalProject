bc1ed13ca7e4a7dc80d03156d6db4ee80ef1778c
RobertGaines <rngaines@ucsc.edu>
2018-03-24 00:35:38 -0700

    All camera pans complete and smooth as butter

--- js/objects/21st-century-cinematography.js
+++ js/objects/21st-century-cinematography.js
@@ -2,27 +2,31 @@ function createPanningCamera(panningPath){
 	
 	var camera = new THREE.PerspectiveCamera( 50, window.innerWidth/window.innerHeight, 0.1, 10000 );
 
-	// eulerX+ = up, eulerY+ = left
+	// ROTATION: eulerX+ = up, eulerY+ = left
 	
 	camera.Start = function(){
 		if (panningPath == 1) {
-			camera.position.set(-14, 3, 25);
+			camera.position.set(-10, 4.1, 35);
 			//camera.setRotationFromAxisAngle(new THREE.Vector3(0, 1, 0), 0.5);
-			camera.setRotationFromEuler(new THREE.Euler(-0.1, 0.9, 0.0, 'YXZ'));
+			camera.setRotationFromEuler(new THREE.Euler(-0.0, 0.9, 0.0, 'YXZ'));
 		} 
 		else if (panningPath == 2) {
-			
+			camera.position.set(10, 14, -30);
+			camera.setRotationFromEuler(new THREE.Euler(0.7, 0.9, 0.0, 'YXZ'));
 		}
 		else if (panningPath == 3) {
-			
+			camera.position.set(-10, 1.5, 4);
 		}
 		else if (panningPath == 4) {
-			
+			camera.position.set(45, 19, 0);
+			camera.setRotationFromEuler(new THREE.Euler(-0.25, 0.5*Math.PI, 0.0, 'YXZ'));
 		}
 		else if (panningPath == 5) {
-			
+			camera.position.set(45, 500, -17);
 		}
 	}
+	
+	// TRANSLATION: X+ = right, Z+ = ???
 
 	camera.AnotherUpdate = function(){
 		var view = camera.rotation.toVector3();
@@ -32,16 +36,19 @@ function createPanningCamera(panningPath){
 			camera.translateOnAxis(new THREE.Vector3(1, 0, 0), 0.1);
 		} 
 		else if (panningPath == 2) {
-			
+			camera.rotateY(0.003);
 		}
 		else if (panningPath == 3) {
-			
+			camera.rotateY(0.004);
+			camera.rotateX(0.0005);
 		}
 		else if (panningPath == 4) {
 			
 		}
 		else if (panningPath == 5) {
-			
+			camera.lookAt(-3, 0, 0);
+			camera.translateOnAxis(new THREE.Vector3(30, -15, 0), 0.1);
+			camera.lookAt(-3, 0, 0);
 		}
 	}
 
